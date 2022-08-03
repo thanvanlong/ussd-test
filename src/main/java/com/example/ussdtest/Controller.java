a7bbe417c31abff511c48f1d6a14bb051ac09a06
longthan <longthan366@gmail.com>
2022-08-03 10:24:58 +0700

    long

--- src/main/java/com/example/ussdtest/Controller.java
+++ src/main/java/com/example/ussdtest/Controller.java
@@ -1,10 +1,7 @@
 package com.example.ussdtest;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
