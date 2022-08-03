487dcdb3e1f8369bd71030cfe03ecd56170d6a3d
longthan <longthan366@gmail.com>
2022-08-03 09:16:41 +0700

    long

--- src/main/java/com/example/ussdtest/Controller.java
+++ src/main/java/com/example/ussdtest/Controller.java
@@ -1,6 +1,7 @@
 package com.example.ussdtest;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @PostMapping("/text/{text}")
+    @GetMapping("/text/{text}")
     public String menu(@PathVariable String text){
 
         System.out.println(text);
 
-        return "CON What is your choice \n 1. My phone number";
+        return "CON What is your choice </br> 1. My phone number";
     }
 
 }
